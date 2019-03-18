787f511a420f96a952ca46a842c948509ce6d3a2
Ish1da <48689461+Ish1da@users.noreply.github.com>
2019-03-18 19:49:30 +0300

    all
    done

---
+++ Launcher/Launcher/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(780, 49);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(798, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Найти";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 42;
+            this.listBox1.Location = new System.Drawing.Point(12, 67);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(780, 760);
+            this.listBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(798, 380);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(210, 60);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Запуск";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1018, 839);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Launcher";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Launcher/Launcher/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+using IWshRuntimeLibrary;
+
+namespace Launcher
+{
+    
+    public partial class Form1 : Form
+    {
+        List<FileInfo> allFoundFiles = new List<FileInfo>();
+        List<Program> pro = new List<Program>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            string dir = System.IO.Path.GetDirectoryName(Application.ExecutablePath); 
+            string serializationFile = Path.Combine(dir, "program.bin");
+            if (System.IO.File.Exists(serializationFile))
+            {
+                using (Stream stream = System.IO.File.Open(serializationFile, FileMode.Open))
+                {
+                    var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+                    pro = (List<Program>)bformatter.Deserialize(stream);
+                }
+            }
+        }
+
+        [Serializable]
+        public class Program
+        {
+            public FileInfo file;
+            public string path;
+            public int count = 0;
+            public Program(FileInfo f)
+            {
+                file = f;
+                path = f.FullName;
+                count = 1;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            DirectoryInfo dirInfo = new DirectoryInfo(textBox1.Text);
+            allFoundFiles = Filef.GetFilesByExtensions(dirInfo, ".exe", ".lnk").ToList();
+            for (int i = 0; i < allFoundFiles.Count; i++)
+            {
+                if (allFoundFiles[i].Extension == ".lnk")
+                {
+                    allFoundFiles[i] = new FileInfo(Filef.lnk(allFoundFiles[i]));
+
+                    bool find = false;
+                    int index = 0;
+                    for (int j = 0; j < pro.Count; j++)
+                    {
+                        if (pro[j].path == allFoundFiles[i].FullName)
+                        {
+                            find = true;
+                            index = j;
+                        }
+                    }
+                    if (find) listBox1.Items.Add(allFoundFiles[i].Name + " Запусков программы: " + pro[index].count);
+                    else listBox1.Items.Add(allFoundFiles[i].Name);
+                }
+                else
+                {
+                    listBox1.Items.Add(allFoundFiles[i].Name);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                FileInfo file = allFoundFiles[listBox1.SelectedIndex];
+                Process iStartProcess = new Process();
+                iStartProcess.StartInfo.FileName = file.ToString();
+                iStartProcess.Start();
+                bool find = false;
+                for (int i = 0; i < pro.Count; i++)
+                {
+                    if (pro[i].path == file.FullName)
+                    {
+                        pro[i].count++;
+                        find = true;
+                    }
+                }
+                if (!find) pro.Add(new Program(allFoundFiles[listBox1.SelectedIndex]));
+
+                listBox1.Items.Clear();
+                DirectoryInfo dirInfo = new DirectoryInfo(textBox1.Text);
+                allFoundFiles = Filef.GetFilesByExtensions(dirInfo, ".exe", ".lnk").ToList();
+                for (int i = 0; i < allFoundFiles.Count; i++)
+                {
+                    if (allFoundFiles[i].Extension == ".lnk")
+                    {
+                        allFoundFiles[i] = new FileInfo(Filef.lnk(allFoundFiles[i]));
+
+                        find = false;
+                        int index = 0;
+                        for (int j = 0; j < pro.Count; j++)
+                        {
+                            if (pro[j].path == allFoundFiles[i].FullName)
+                            {
+                                find = true;
+                                index = j;
+                            }
+                        }
+                        if (find) listBox1.Items.Add(allFoundFiles[i].Name + " Запусков программы: " + pro[index].count);
+                        else listBox1.Items.Add(allFoundFiles[i].Name);
+                    }
+                    else
+                    {
+                        listBox1.Items.Add(allFoundFiles[i].Name);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка при запуске программы");
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            string dir = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            string serializationFile = Path.Combine(dir, "program.bin");
+            using (Stream stream = System.IO.File.Open(serializationFile, FileMode.OpenOrCreate))
+            {
+                var bformatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+
+                bformatter.Serialize(stream, pro);
+            }
+        }
+    }
+
+
+
+    public static class Filef
+    {
+        public static IEnumerable<FileInfo> GetFilesByExtensions(this DirectoryInfo dir, params string[] extensions)
+        {
+            if (extensions == null)
+                throw new ArgumentNullException("extensions");
+            IEnumerable<FileInfo> files = dir.EnumerateFiles();
+            return files.Where(f => extensions.Contains(f.Extension));
+        }
+
+        public static string lnk(FileInfo linkf)
+        {
+            if (System.IO.File.Exists(linkf.FullName))
+            {
+                WshShell shell = new WshShell();
+                IWshShortcut link = (IWshShortcut)shell.CreateShortcut(linkf.FullName);
+
+                return(link.TargetPath);
+            }
+            MessageBox.Show("error");
+            return linkf.ToString();
+        }
+    }
+}
